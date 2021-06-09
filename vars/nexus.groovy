efeedbd6cefe954996dd8e3b9f450bb3b15a8893
Manmohan506 <manmohan@zelarsoft.com>
2021-06-09 17:54:54 +0530

    f

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -38,7 +38,7 @@ def code_build(APP_TYPE, COMPONENT) {
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   } else if(APP_TYPE == "GOLANG") {
-    command = "go build"
+    command = "go grt -d && go build"
     def execute_com=sh(returnStdout: true, script: command)
     print execute_com
   } else if(APP_TYPE == "NGINX") {
