41c223683cdaa358e676f96ccb5393114487d206
Manmohan506 <manmohan@zelarsoft.com>
2021-06-09 16:55:46 +0530

    as

--- vars/todoshop.groovy
+++ vars/todoshop.groovy
@@ -1,59 +1,48 @@
-def call(Map params = [:]) {
-  
-  def args = [
-          NEXUS_IP               : '172.31.13.99',
-  ]
-  args << params
-
-  
-  pipeline {
-    agent {
-      label "${args.SLAVE_LABEL}"
-    }
-
-    triggers {
-      pollSCM('* * * * 1-5')
-    }
-
-    environment {
-      COMPONENT     = "${args.COMPONENT}"
-      NEXUS_IP      = "${args.NEXUS_IP}"
-      PROJECT_NAME  = "${args.PROJECT_NAME}"
-      SLAVE_LABEL   = "${args.SLAVE_LABEL}"
-      APP_TYPE      = "${args.APP_TYPE}"
-    }
-
-    stages {
-
-      stage('Build Code & Install Dependencies') {
-        steps {
-          script {
-            build = new nexus()
-            build.code_build("${APP_TYPE}", "${COMPONENT}")
-          }
+def call(Map params = [:] ) {
+     def args = [
+        NEXUS_IP : '172.31.13.99', 
+     ]
+     args << params
+    pipeline {
+        agent {
+            label "${args.SLAVE_LABEL}"
         }
-      }
-
-
-      stage('Prepare Artifacts') {
-        steps {
-          script {
-            prepare = new nexus()
-            prepare.make_artifacts("${APP_TYPE}", "${COMPONENT}")
-          }
+    
+        environment {
+            COMPONENT ="${args.COMPONENT}"
+            NEXUS_IP = "${args.NEXUS_IP}"
+            PROJECT_NAME = "${args.PROJECT_NAME}"
+            SLAVE_LABEL = "${args.SLAVE_LABEL}"
+            APP_TYPE    = "${args.APP_TYPE}"
         }
-      }
+        stages {
+            stage('Build code & install dependencies') {
+                steps {
+                    script {
+                        build = new nexus()
+                        build.code_build("${APP_TYPE}","${COMPONENT}")
+                    }
+                }
 
-      stage('Upload Artifacts') {
-        steps {
-          script {
-            prepare = new nexus()
-            prepare.nexus(COMPONENT)
-          }
+            }
+            stage('Prepare Artifacts') {
+
+                steps {
+                    script {
+                        prepare = new nexus()
+                        prepare.make_artifacts("${APP_TYPE}","${COMPONENT}")
+                    }
+                }
+            }
+
+            stage('Upload Artifacts') {
+                steps {
+                    script {
+                        prepare = new nexus()
+                        prepare.nexus(COMPONENT)
+                    }
+
+                }
+            }
         }
-      }
-
     }
-
-  }
-}
