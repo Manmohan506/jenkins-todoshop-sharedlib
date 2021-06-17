a56f34f4497186d6b8d4e930bee8ada3a447439f
Manmohan506 <manmohan@zelarsoft.com>
2021-06-17 15:05:34 +0530

    gf

--- seed.groovy
+++ seed.groovy
@@ -24,13 +24,13 @@ for (i in 0..count) {
             'hudson.plugins.git.UserRemoteConfig' {
               'url'('https://github.com/Manmohan506/'+j+'.git')
               'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-              
+
 
             }
           }
           'branches' {
             'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags')
+              'name'('*/tags/*')
             }
           }
         }
